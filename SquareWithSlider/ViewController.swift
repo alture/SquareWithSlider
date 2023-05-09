538a17118e919da7be82f2fecac579e6ab750ebf
Alisher Altore <a.altore@jusan.kz>
2023-05-10 00:24:25 +0600

    use property animator

--- SquareWithSlider/ViewController.swift
+++ SquareWithSlider/ViewController.swift
@@ -9,78 +9,62 @@ import UIKit
 
 final class ViewController: UIViewController {
     
+    private let animator = UIViewPropertyAnimator(duration: 0.7, curve: .easeOut)
     private lazy var squareView: UIView = {
-        let view = UIView()
+        let view = UIView(frame: CGRect(
+            x: view.layoutMargins.left,
+            y: 110,
+            width: Constants.squareViewSize,
+            height: Constants.squareViewSize))
         view.backgroundColor = .systemBlue
+        view.layer.cornerCurve = .continuous
         view.layer.cornerRadius = 8
-        view.clipsToBounds = true
-        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     private lazy var slider: UISlider = {
-        let slider = UISlider()
+        let slider = UISlider(frame: CGRect(
+            x: view.layoutMargins.left,
+            y: squareView.frame.maxY + 40,
+            width: view.frame.width - view.layoutMargins.left - view.layoutMargins.right,
+            height: 10))
         slider.minimumValue = 0
         slider.maximumValue = 1.0
         slider.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
         slider.addTarget(self, action: #selector(sliderTouchUp(_:)), for: .touchUpInside)
-        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.addTarget(self, action: #selector(sliderTouchUp(_:)), for: .touchUpInside)
         return slider
     }()
     
-    private lazy var squareViewLeadingConstraint: NSLayoutConstraint! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+         
+        animator.pausesOnCompletion = true
+        animator.addAnimations {
+            self.squareView.frame.origin.x = self.view.frame.width - self.view.layoutMargins.left  - self.squareView.frame.width
+            self.squareView.transform  = .init(scaleX: 1.5, y: 1.5).rotated(by: .pi / 2)
+        }
     }
 
     private func setupView() {
         view.backgroundColor = .white
         view.addSubview(squareView)
         view.addSubview(slider)
-        
-        squareViewLeadingConstraint = squareView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
-        
-        NSLayoutConstraint.activate([
-            squareViewLeadingConstraint,
-            squareView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
-            squareView.widthAnchor.constraint(equalToConstant: Constants.squareViewSize),
-            squareView.heightAnchor.constraint(equalToConstant: Constants.squareViewSize),
-            
-            slider.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            slider.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            slider.topAnchor.constraint(equalTo: squareView.bottomAnchor, constant: 40),
-            slider.heightAnchor.constraint(equalToConstant: 40)
-        ])
     }
     
     @objc private func sliderValueChanged(_ sender: UISlider) {
-        let angle = CGFloat(sender.value) * .pi / 2
-        let scale = min(CGFloat(sender.value) + 1.0, 1.5)
-        let translation = CGFloat(sender.value) * (sender.frame.width - Constants.squareViewSize * Constants.squareViewScaleValue)
-        let rotationTransform = CGAffineTransform(rotationAngle: angle)
-        let scaleTransform = CGAffineTransform(scaleX: scale, y: scale)
-        
-        squareViewLeadingConstraint.constant = translation
-        UIView.animate(withDuration: 0.4, delay: 0, options: [.allowUserInteraction, .beginFromCurrentState]) {
-            self.squareView.transform = rotationTransform.concatenating(scaleTransform)
-            self.view.layoutIfNeeded()
-        }
+        animator.fractionComplete = CGFloat(sender.value)
     }
     
     @objc private func sliderTouchUp(_ sender: UISlider) {
-        guard sender.value != sender.maximumValue else {
-            return
-        }
-        
+        animator.startAnimation()
         sender.setValue(sender.maximumValue, animated: true)
-        self.sliderValueChanged(sender)
     }
 }
 
 enum Constants {
-    static var squareViewSize: CGFloat = 70
+    static var squareViewSize: CGFloat = 80
     static var squareViewScaleValue: CGFloat = 1.5
 }
-
